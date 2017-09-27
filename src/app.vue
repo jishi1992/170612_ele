a4912d2788e8ebc7b0a3c780268cda1049ff5ff1
zxfjd3g <258147149@qq.com>
2017-09-27 17:35:41 +0800

    init app

---
+++ src/app.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ele-header></ele-header>
+    <div class="tab">
+      <div class="tab-item">
+        <!--生成路由链接的a标签-->
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    <!--用来标识显示当前路由组件的标签-->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+
+  export default {
+    components: {
+      'ele-header': header
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab
+    .tab-item
+      display inline-block
+      a
+        text-decoration none
+        &.router-link-active
+          color: red
+</style>
